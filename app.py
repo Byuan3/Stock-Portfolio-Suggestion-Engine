003871e7df68fa7721cc6a6f883f74483c0366e6
Byuan3 <byuan@ucsd.edu>
2022-05-22 17:03:59 -0700

    update

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
+portfolio = 0.0
+data = []
 
 
-@app.route('/')
-def index():  # put application's code here
-    return render_template('index.html')
+@app.route('/', methods=['GET'])
+def index():
+    global portfolio, data
+    return render_template('index.html', portfolio=portfolio, data=data)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    global portfolio
+    if request.method == 'POST':
+        amount = request.form.get('amount')
+        e = request.form.get('Ethical')
+        g = request.form.get('Growth')
+        i = request.form.get('Index')
+        q = request.form.get('Quality')
+        v = request.form.get('Value')
+        if amount.isnumeric():
+            portfolio += float(amount)
+            flash('Add $' + amount)
+    return redirect('/')
+
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    global portfolio
+    if request.method == 'POST':
+        portfolio = 0
+        flash('Your portfolio is reset!')
+    return redirect('/')
 
 
 if __name__ == '__main__':
--- static/js/chart.js
+++ static/js/chart.js
@@ -5,7 +5,7 @@ const myChart = new Chart(ctx, {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: 'test1',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [15, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
